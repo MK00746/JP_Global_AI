87f71c8c0bed689161ab921c02e4bb420f6866f1
MK00746 <S65888@ocean.umt.edu.my>
2025-11-05 14:38:04 +0800

    Fix port binding for Render

--- app.py
+++ app.py
@@ -1856,5 +1856,9 @@ ADMIN_HTML = """
 </html>
 """
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
